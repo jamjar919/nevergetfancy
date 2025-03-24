1371bc9f6ed845add8c3d0f4314b43dea069d8c8
James Paterson <james@mjsquare.com>
2025-03-24 13:14:02 +0000

    More aggressive indexer

--- src/server/fpl/index/indexTeams.ts
+++ src/server/fpl/index/indexTeams.ts
@@ -3,6 +3,8 @@ import {fetchFromApi} from "../../util/fetchFromApi";
 import {FantasyPremierLeagueApi} from "../api/apiConfig";
 import {FantasyManagerId} from "../../../graphql/Reference";
 
+const dao = FplTeamsDao.getInstance();
+
 /**
  * The FPL api does not provide any native search functionality by team name, which is massively annoying
  * Thankfully, the team id is sequential, so we can pre-fetch all the teams and then store them in a database
@@ -12,11 +14,18 @@ import {FantasyManagerId} from "../../../graphql/Reference";
  * and can save you some bandwidth.
  */
 const indexTeams = async () => {
-    const dao = FplTeamsDao.getInstance();
 
-    const batchSize = 1_000_000;
+    // Total number of teams to scan
+    const seekNumber = 1_000_000;
+
+    // Number of synchronous requests to make at once
+    const batchSize = 10;
+
+    // How often to report progress
+    const reportingInterval = batchSize * 10;
+
     const start = dao.getMaxTeamId() + 1;
-    const end = start + batchSize;
+    const end = start + seekNumber;
 
     console.log("Indexing teams...");
     console.log("This will take a while");
@@ -24,33 +33,47 @@ const indexTeams = async () => {
 
     let numNotFound = 0;
 
-    for (let i = start; i < end; i += 1) {
-        if (i % 100 === 0) {
+    for (let i = start; i < end; i += batchSize) {
+        if ((i - start) % reportingInterval === 0) {
             console.log(`${i}/${end}`);
             if (numNotFound > 0) {
-                console.log(`[warn] Could not find ${numNotFound} teams in this batch`);
+                console.log(`[warn] Could not find ${numNotFound} teams in last 100`);
             }
             numNotFound = 0;
         }
 
-        const team = await fetchFromApi(FantasyPremierLeagueApi.Manager(String(i) as FantasyManagerId))
-            .then((res) => res.json())
-            .catch(() => ({}));
-
-        if (!team?.id && !team?.name) {
-            numNotFound++;
-            dao.addNotFoundTeam(i);
-            continue;
+        const promises = [];
+        for (let j = 0; j < batchSize; j++) {
+            promises.push(processSingleTeam(i + j).then((found) => {
+                if (!found) {
+                    numNotFound++;
+                }
+            }));
         }
 
-        const playerName = `${team.player_first_name} ${team.player_last_name}`;
-
-        dao.addTeam(
-            team.id,
-            team.name,
-            playerName,
-        );
+        await Promise.all(promises)
     }
+}
+
+const processSingleTeam = async (teamId: number): Promise<boolean> => {
+    const team = await fetchFromApi(FantasyPremierLeagueApi.Manager(String(teamId) as FantasyManagerId))
+        .then((res) => res.json())
+        .catch(() => ({}));
+
+    if (!team?.id && !team?.name) {
+        dao.addNotFoundTeam(teamId);
+        return false;
+    }
+
+    const playerName = `${team.player_first_name} ${team.player_last_name}`;
+
+    dao.addTeam(
+        team.id,
+        team.name,
+        playerName,
+    );
+
+    return true;
 }
 
 export { indexTeams }
