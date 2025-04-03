378978232093d2054a439bc2405dfd168dfc493a
James Paterson <james@mjsquare.com>
2025-04-03 13:40:45 +0100

    Decomplicate captain

--- src/server/fpl/fancy/fancyCalculator.ts
+++ src/server/fpl/fancy/fancyCalculator.ts
@@ -1,6 +1,5 @@
 import { EventId, FantasyManagerId } from '../../../graphql/Reference';
-import { FancyPickLine, FancyResult } from '../../../graphql/generated/Resolver';
-import { convertGameSummary } from '../../resolver/converter/convertGameSummary';
+import { FancyResult } from '../../../graphql/generated/Resolver';
 import { getArrayWithNumbersInRange } from '../../util/getArrayWithNumbersInRange';
 import { getManager } from '../api/manager/getManager';
 import { getGameweekHistory } from '../api/picks/getGameweekHistory';
@@ -19,29 +18,13 @@ const fancyCalculator = async (managerId: FantasyManagerId): Promise<FancyResult
     );
 
     // Get the total scores for the captain
-    const captainScores = await getCaptainScoresByWeek(history);
-
-    // Convert captain scores to an array
-    const captainScoresAsArray: FancyPickLine[] = Object.entries(captainScores)
-        .map(([gameweek, performance]) => {
-            const {
-                captain: { captainId, wasOriginallyViceCaptain },
-                gameSummary,
-            } = performance;
-
-            return {
-                gameweek: parseInt(gameweek),
-                captainId,
-                wasOriginallyViceCaptain,
-                captainGameSummary: gameSummary ? convertGameSummary(gameSummary) : undefined,
-            };
-        })
+    const captainScores = (await getCaptainScoresByWeek(history))
         .sort((a, b) => b.gameweek - a.gameweek);
 
     // WHERE ARE THE COMPARISONS??
     // (they're handled in the resolver)
     return {
-        captainScores: captainScoresAsArray,
+        captainScores,
     };
 };
 
--- src/server/fpl/fancy/gameweek/getCaptainPicksByWeek.ts
+++ src/server/fpl/fancy/gameweek/getCaptainPicksByWeek.ts
@@ -1,27 +1,17 @@
 import { EventId } from '../../../../graphql/Reference';
 import { getPlayerPreviousGame } from '../../api/player/getPlayerPreviousGame';
 import { GameweekHistoryDto } from '../../api/type/GameweekHistoryDto';
-import { PlayerPreviousGameDto } from '../../api/type/PlayerPreviousGameDto';
 import { getCaptainForGameweek } from './getCaptainForGameweek';
-import { CaptainDto } from './type/CaptainDto';
-
-type CaptainSummaryForGameWeek = {
-    captain: CaptainDto;
-    gameSummary?: PlayerPreviousGameDto;
-};
-
-type CaptainSummaryPerWeek = { [key: EventId]: CaptainSummaryForGameWeek };
+import { convertGameSummary } from '../../../resolver/converter/convertGameSummary';
+import { FancyPickLine } from '../../../../graphql/generated/Resolver';
 
 /**
  * Get the results by gameweek for a team
  */
 const getCaptainScoresByWeek = async (
     weeks: GameweekHistoryDto[]
-): Promise<CaptainSummaryPerWeek> => {
-    const captainScoresByWeek: CaptainSummaryPerWeek = {};
-
-    // Get the performance of the player's selected captain for each week
-    const performances = await Promise.all(
+): Promise<FancyPickLine[]> => {
+    return await Promise.all(
         weeks.map(async (week) => {
             const eventId: EventId = week.gameweek;
             const captain = getCaptainForGameweek(week);
@@ -29,21 +19,12 @@ const getCaptainScoresByWeek = async (
 
             return {
                 gameweek: eventId,
-                captain,
-                gameSummary,
+                captainId: captain.captainId,
+                wasOriginallyViceCaptain: captain.wasOriginallyViceCaptain,
+                captainGameSummary: gameSummary ? convertGameSummary(gameSummary) : undefined,
             };
         })
     );
-
-    // Convert to map
-    performances.forEach((week) => {
-        captainScoresByWeek[week.gameweek] = {
-            captain: week.captain,
-            gameSummary: week.gameSummary,
-        };
-    });
-
-    return captainScoresByWeek;
 };
 
 export { getCaptainScoresByWeek };
