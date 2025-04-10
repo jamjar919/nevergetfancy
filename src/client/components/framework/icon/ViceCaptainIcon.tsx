3a021bb3ecea7d4d1acfc9f9d78eab6efcee3f2a
James Paterson <james@mjsquare.com>
2025-04-10 18:54:36 +0100

    Icons

---
+++ src/client/components/framework/icon/ViceCaptainIcon.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+interface ViceCaptainIconProps {
+  className?: string;
+  size?: number;
+  color?: string;
+}
+
+const ViceCaptainIcon: React.FC<ViceCaptainIconProps> = ({
+  className = '',
+  size = 24,
+  color = 'currentColor'
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      role="img"
+      focusable="false"
+      className={className}
+    >
+      <title>Vice Captain</title>
+      <circle cx="12" cy="12" r="12" aria-hidden="true"></circle>
+      <polygon
+        points="13.5 .375 8.925 12.375 4.65 12.375 0 .375 3.15 .375 6.75 10.05 10.35 .375"
+        transform="translate(5.25 6)"
+        fill={color}
+        aria-hidden="true"
+      ></polygon>
+    </svg>
+  );
+};
+
+export default ViceCaptainIcon;
