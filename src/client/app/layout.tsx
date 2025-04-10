f7c6ccb4b1c142e2d642ab867f23ff0687bd753f
James Paterson <james@mjsquare.com>
2025-04-10 18:08:35 +0100

    Favicon

--- src/client/app/layout.tsx
+++ src/client/app/layout.tsx
@@ -22,7 +22,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     data-domain="nevergetfancy.com"
                     src="https://plausible.io/js/script.tagged-events.js"
                 ></script>
-                <title>Never Get Fancy</title>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
             </head>
             <body>
                 <ApolloWrapper>
