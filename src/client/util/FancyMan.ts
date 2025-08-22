fd3a7624f404af730bb234ecf5bb074b817ed5bf
James Paterson <james@mjsquare.com>
2025-08-22 13:11:42 +0100

    Update Salah/Haaland ids

--- src/client/util/FancyMan.ts
+++
@@ -1,5 +0,0 @@
-import { PremierLeaguePlayerId } from '../../graphql/Reference';
-
-const SALAH_PLAYER_ID: PremierLeaguePlayerId = '328' as PremierLeaguePlayerId;
-
-export { SALAH_PLAYER_ID };
--- src/server/fpl/fancy/fancyMan.ts
+++ src/server/fpl/fancy/fancyMan.ts
@@ -1,6 +1,6 @@
 import { PremierLeaguePlayerId } from '../../../graphql/Reference';
 
-const SALAH_PLAYER_ID: PremierLeaguePlayerId = '328' as PremierLeaguePlayerId;
-const HAALAND_PLAYER_ID: PremierLeaguePlayerId = '351' as PremierLeaguePlayerId;
+const SALAH_PLAYER_ID: PremierLeaguePlayerId = '381' as PremierLeaguePlayerId;
+const HAALAND_PLAYER_ID: PremierLeaguePlayerId = '430' as PremierLeaguePlayerId;
 
 export { SALAH_PLAYER_ID, HAALAND_PLAYER_ID };
